d167a00bea1c3b9d0ca7592a480142014e4de85e
yanby <yanby@ziroom.com>
2025-03-20 11:00:36 +0800

    refactor: 移除未使用的useRouter导入
    移除DatePicker组件中未使用的useRouter导入，以保持代码简洁

--- app/components/DatePicker.tsx
+++ app/components/DatePicker.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { useRouter } from 'next/navigation';
 import styles from './DatePicker.module.css';
 import { useTelegramDate } from '../contexts/TelegramDateContext';
 
